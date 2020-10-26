f70d68cf83ff1dc0218b4ce7a474a14b0c021b58
David <42513699+David1105@users.noreply.github.com>
2020-10-26 14:12:43 +0300

    Add files via upload

--- website/portal/admin.py
+++ website/portal/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Discipline, GroupSt, Kafedra, Prepod, ResultOfMidterms, Semester, Student
 
-# Register your models here.
+admin.site.register(Discipline)
+admin.site.register(GroupSt)
+admin.site.register(Kafedra)
+admin.site.register(Prepod)
+admin.site.register(ResultOfMidterms)
+admin.site.register(Semester)
+admin.site.register(Student)
