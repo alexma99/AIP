59c4e8611b233452d0ee1446fc6db87058e4018b
David <42513699+David1105@users.noreply.github.com>
2020-11-11 20:04:00 +0200

    Add files via upload

--- website/website/uploadcsv.py
+++ website/website/uploadcsv.py
@@ -29,11 +29,11 @@ def csv(filename, cur):
 
 def page(request):
     con = psycopg2.connect(
-        database="btj4y9qiryp7r9sizol0",
-        user="uuugtksqbcxtzbutebat",
-        password="VOLUCrmJtrbgdruOKPWw",
-        host="btj4y9qiryp7r9sizol0-postgresql.services.clever-cloud.com",
-        port="5432"
+        database = "btj4y9qiryp7r9sizol0",
+        user = "uuugtksqbcxtzbutebat",
+        password = "VOLUCrmJtrbgdruOKPWw",
+        host = "btj4y9qiryp7r9sizol0-postgresql.services.clever-cloud.com",
+        port = "5432"
     )
     cur = con.cursor()
     cur.execute('''SET session_replication_role = 'replica';''')  # откл проверку ключей
