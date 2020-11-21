ee2a5e42b97b01f8d5ce2d7cffecf8e46754d21b
David1105 <42513699+David1105@users.noreply.github.com>
2020-11-21 16:39:29 +0200

    Add files via upload

--- website/website/uploadcsv.py
+++ website/website/uploadcsv.py
@@ -1,34 +1,19 @@
 from django.http import HttpResponse
+from django.db import IntegrityError
 from sqlalchemy import create_engine
 import pandas as pd
-
-
-def is_number(strng):
-    try:
-        float(strng)
-        return True
-    except ValueError:
-        return False
+import time
 
 
 def csv(filename, engine):
-    filepath = r"./csv/"+filename
-    df = pd.read_csv(filepath, header=None)
-    n = df.iloc[:, 1].size
-    m = df.iloc[1, :].size
-    for k in range(0, n):
-        string = ""
-        for j in range(0, m):
-            if is_number(df.iloc[k, j]):
-                string += str(df.iloc[k, j]) + ", "
-            else:
-                string += "'" + str(df.iloc[k, j]) + "', "
-        engine.execute('''INSERT INTO '''+filename+''' VALUES ('''+string[:-2]+''');''')
+    filepath = r"./csv/"+filename+".csv"
+    df = pd.read_csv(filepath, sep=';', index_col=0)
+    df.to_sql(filename, engine, if_exists="append")
+    time.sleep(3)
     return
 
 
 def page(request):
-
     database = "btj4y9qiryp7r9sizol0"
     user = "uuugtksqbcxtzbutebat"
     pwd = "VOLUCrmJtrbgdruOKPWw"
@@ -45,12 +30,14 @@ def page(request):
     )
 
     engine = create_engine(conn_string)
-
-    csv('discipline', engine)
-    csv('group_st', engine)
-    csv('kafedra', engine)
-    csv('prepod', engine)
-    csv('semester', engine)
-    csv('student', engine)
-    engine.close()
-    return HttpResponse('Данные загружены')
+    try:
+        csv('semester', engine)
+        csv('kafedra', engine)
+        csv('discipline', engine)
+        csv('prepod', engine)
+        csv('group_st', engine)
+        csv('student', engine)
+        csv('result_of_midterms', engine)
+    except IntegrityError:
+        return HttpResponse('Данные не удалось загрузить')
+    return HttpResponse('Данные успешно загружены')
