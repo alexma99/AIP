778c2f6bf3811b3861f3492e41abb61a019065e4
David1105 <42513699+David1105@users.noreply.github.com>
2020-12-06 20:25:17 +0200

    Add files via upload

--- website/api.py
+++ website/api.py
@@ -5,7 +5,7 @@ from django.core.management import execute_from_command_line
 execute_from_command_line(sys.argv)
 from fastapi import FastAPI
 import uvicorn
-from portal.models import Discipline
+from portal.models import *
 from django.forms.models import model_to_dict
 
 
@@ -25,37 +25,37 @@ def read_item(id_disc):
 
 @app.get("/group-st/{id_group}")
 def read_item(id_group):
-    disc = Discipline.objects.get(pk=id_group)
+    disc = GroupSt.objects.get(pk=id_group)
     return model_to_dict(disc)
 
 
 @app.get("/kafedra/{id_kaf}")
 def read_item(id_kaf):
-    disc = Discipline.objects.get(pk=id_kaf)
+    disc = Kafedra.objects.get(pk=id_kaf)
     return model_to_dict(disc)
 
 
 @app.get("/prepod/{id_prep}")
 def read_item(id_prep):
-    disc = Discipline.objects.get(pk=id_prep)
+    disc = Prepod.objects.get(pk=id_prep)
     return model_to_dict(disc)
 
 
 @app.get("/result-of-midterms/{id_per}")
 def read_item(id_per):
-    disc = Discipline.objects.get(pk=id_per)
+    disc = ResultOfMidterms.objects.get(pk=id_per)
     return model_to_dict(disc)
 
 
 @app.get("/semester/{id_sem}")
 def read_item(id_sem):
-    disc = Discipline.objects.get(pk=id_sem)
+    disc = Semester.objects.get(pk=id_sem)
     return model_to_dict(disc)
 
 
 @app.get("/student/{id_stud}")
 def read_item(id_stud):
-    disc = Discipline.objects.get(pk=id_stud)
+    disc = Student.objects.get(pk=id_stud)
     return model_to_dict(disc)
 
 
