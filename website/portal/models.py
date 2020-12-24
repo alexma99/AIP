df662303ad5e5f6eca60b93c9485a08231059807
David1105 <42513699+David1105@users.noreply.github.com>
2020-12-24 20:01:42 +0200

    Add files via upload

--- website/portal/models.py
+++ website/portal/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Discipline(models.Model):
     id_disc = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=20, blank=True, null=True)
-    id_kaf = models.ForeignKey('Kafedra', models.DO_NOTHING, db_column='id_kaf', blank=True, null=True)
+    id_kaf = models.ForeignKey('Kafedra', models.CASCADE, db_column='id_kaf', blank=True, null=True)
 
     class Meta:
         db_table = 'discipline'
@@ -13,7 +13,7 @@ class Discipline(models.Model):
 class GroupSt(models.Model):
     id_group = models.BigAutoField(primary_key=True)
     course = models.IntegerField(blank=True, null=True)
-    id_kaf = models.ForeignKey('Kafedra', models.DO_NOTHING, db_column='id_kaf', blank=True, null=True)
+    id_kaf = models.ForeignKey('Kafedra', models.CASCADE, db_column='id_kaf', blank=True, null=True)
 
     class Meta:
         db_table = 'group_st'
@@ -34,7 +34,7 @@ class Prepod(models.Model):
     last_name = models.CharField(max_length=20, blank=True, null=True)
     otchestvo = models.CharField(max_length=20, blank=True, null=True)
     degree = models.CharField(max_length=20, blank=True, null=True)
-    id_disc = models.ForeignKey(Discipline, models.DO_NOTHING, db_column='id_disc', blank=True, null=True)
+    id_disc = models.ForeignKey(Discipline, models.CASCADE, db_column='id_disc', blank=True, null=True)
 
     class Meta:
         db_table = 'prepod'
@@ -44,10 +44,10 @@ class ResultOfMidterms(models.Model):
     id_per = models.BigAutoField(primary_key=True)
     mark = models.IntegerField(blank=True, null=True)
     date = models.DateField(blank=True, null=True)
-    id_stud = models.ForeignKey('Student', models.DO_NOTHING, db_column='id_stud', blank=True, null=True)
-    id_sem = models.ForeignKey('Semester', models.DO_NOTHING, db_column='id_sem', blank=True, null=True)
-    id_disc = models.ForeignKey(Discipline, models.DO_NOTHING, db_column='id_disc', blank=True, null=True)
-    id_prep = models.ForeignKey(Prepod, models.DO_NOTHING, db_column='id_prep', blank=True, null=True)
+    id_stud = models.ForeignKey('Student', models.CASCADE, db_column='id_stud', blank=True, null=True, )
+    id_sem = models.ForeignKey('Semester', models.CASCADE, db_column='id_sem', blank=True, null=True)
+    id_disc = models.ForeignKey(Discipline, models.CASCADE, db_column='id_disc', blank=True, null=True)
+    id_prep = models.ForeignKey(Prepod, models.CASCADE, db_column='id_prep', blank=True, null=True)
 
     class Meta:
         db_table = 'result_of_midterms'
@@ -67,7 +67,7 @@ class Student(models.Model):
     last_name = models.CharField(max_length=20, blank=True, null=True)
     otchestvo = models.CharField(max_length=20, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
-    id_group = models.ForeignKey(GroupSt, models.DO_NOTHING, db_column='id_group', blank=True, null=True)
+    id_group = models.ForeignKey(GroupSt, models.CASCADE, db_column='id_group', blank=True, null=True)
 
     class Meta:
         db_table = 'student'
